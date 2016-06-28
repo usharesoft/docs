df53665b2b411331a8f5b181e8e49f4af6c31e76
usharesoft <sysadmin@usharesoft.com>
2016-06-28 19:40:32 +0200

    Update conf.py

--- toc/source/conf.py
+++ toc/source/conf.py
@@ -53,9 +53,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'UShareSoft Docs'
-copyright = u'2016, usharesoft'
-author = u'usharesoft'
+project = u'UForge Docs'
+copyright = u'2016 FUJITSU LIMITED'
+author = u'FUJITSU'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -209,7 +209,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'UShareSoftDocsdoc'
+htmlhelp_basename = 'UForgeDocsdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -231,8 +231,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'UShareSoftDocs.tex', u'UShareSoft Documentation',
-     u'usharesoft', 'manual'),
+    (master_doc, 'UForgeDocs.tex', u'UForge Documentation',
+     u'FUJITSU', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -261,7 +261,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'usharesoftdocs', u'UShareSoft Documentation',
+    (master_doc, 'uforgedocs', u'UForge Documentation',
      [author], 1)
 ]
 
@@ -275,8 +275,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'UShareSoftDocs', u'UShareSoft Documentation',
-     author, 'UShareSoftDocs', 'UShareSoft Documentation',
+    (master_doc, 'UForgeDocs', u'UForge Documentation',
+     author, 'UForgeDocs', 'UForge Documentation',
      'Miscellaneous'),
 ]
 
